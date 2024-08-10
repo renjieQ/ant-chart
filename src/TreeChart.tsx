c197111d1158dbdc7924ed5d47ac209cf01faa60
renjieQ <qurenjie@qq.com>
2024-08-10 17:39:53 +0800

    echart tree

---
+++ src/TreeChart.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import * as echarts from 'echarts';
+import data from './data.json'; // 导入本地 JSON 文件
+
+const TreeChart = () => {
+  const chartRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+
+    data.children.forEach((datum: any, index) => {
+      index % 2 === 0 && (datum.collapsed = true);
+    });
+    myChart.setOption({
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+      series: [
+        {
+          type: 'tree',
+          data: [data],
+          top: '1%',
+          left: '7%',
+          bottom: '1%',
+          right: '20%',
+          symbolSize: 7,
+          label: {
+            position: 'left',
+            verticalAlign: 'middle',
+            align: 'right',
+            fontSize: 9,
+          },
+          leaves: {
+            label: {
+              position: 'right',
+              verticalAlign: 'middle',
+              align: 'left',
+            },
+          },
+          emphasis: {
+            focus: 'descendant',
+          },
+          expandAndCollapse: true,
+          animationDuration: 550,
+          animationDurationUpdate: 750,
+        },
+      ],
+    });
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} style={{ width: '100%', height: '400px' }} />;
+};
+
+export default TreeChart;
